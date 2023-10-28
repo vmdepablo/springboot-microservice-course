309923b18e161a4805c5ea21f20493247399a34f
Víctor Manuel de Pablo Alvir <depablovictormanuel@gmail.com>
2023-10-28 16:54:18 +0200

    Data model

---
+++ course/src/main/java/microservice/course/course/BeerMicroService.java
@@ -0,0 +1,14 @@
+package microservice.course.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerMicroService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerMicroService.class, args);
+	}
+
+}
+
---
+++ course/src/main/java/microservice/course/course/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package microservice.course.course.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
